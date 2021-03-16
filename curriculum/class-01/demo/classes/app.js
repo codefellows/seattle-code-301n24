89ebaebf1e862f5150fc2d0bd087c2f768f2611a
Lena Eivy <applena@gmail.com>
2021-03-16 12:23:44 -0700

    day 1 demo updated

--- curriculum/class-01/demo/classes/app.js
+++ curriculum/class-01/demo/classes/app.js
@@ -25,20 +25,3 @@ const spot = new Dog({name:'Spot', color:['black', 'white'], age:10}, 'dalmation
 
 spot.speak();
 spot.goodBoi();
-
-////////////////////
-
-class Dog {
-  constructor(name, color, age, type) {
-    this.name = name;
-    this.color = color;
-    this.age = age;
-    this.type = type;
-  }
-
-  speak = () => console.log(`${this.name} is a $${this.tyep}`);
-
-  // speak = function(){
-  //   console.log(`${this.name} is a $${this.tyep}`)
-  // }
-}
