a37bb8bdc170875e2fffe65d4d2b3ffb283560cc
Lena Eivy <applena@gmail.com>
2021-03-28 18:27:41 -0700

    solution code updated for lab 13

--- curriculum/class-13/solution/front-end/src/AddABookButton.js
+++ curriculum/class-13/solution/front-end/src/AddABookButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BookFormModal from './bookFormModal';
+import BookFormModal from './BookFormModal';
 
 class AddABookButton extends React.Component {
   constructor(props){
