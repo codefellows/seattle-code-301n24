864ae013e4b1ad74deb5edb2a7fd286b47fea580
Lena Eivy <applena@gmail.com>
2021-04-13 11:14:08 -0700

    class 14 demo and faciltator docs updated

--- curriculum/class-13/demo/back-end/models/Users.js
+++ curriculum/class-13/demo/back-end/models/Users.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 
 const giftSchema = new mongoose.Schema({
-  name: {type: String}
+  name: {type: String},
+  description: {type: String}
 });
 
 const birthdayPerson = new mongoose.Schema({
