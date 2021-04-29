473d915d388741cb7427f2061a1daaf0c49e7270
Lena Eivy <applena@gmail.com>
2021-04-29 09:16:29 -0700

    updated wording on challenge 11.6 to make it more clear

--- curriculum/class-11/challenges/challenges-11.test.js
+++ curriculum/class-11/challenges/challenges-11.test.js
@@ -135,7 +135,7 @@ let findMaleAndFemale = (data) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6 
 
-Write a function named findShortest that, given the Star Wars data from Challenge 6, uses any combination of filter, map and reduce to return the name of the shortest character.
+Write a function named findShortest that, given the Star Wars data from Challenge 6, uses any combination of filter, map and reduce to return the name of the character who is the shortest in height.
 ------------------------------------------------------------------------------------------------ */
 
 let findShortest = (data) => {
