a0f1998437d052567c9ed28d33257e1d22dc91e1
JB-Tellez <jb.tellez@gmail.com>
2021-08-13 14:47:45 -0700

    adjusts front-end demo to match latest requirements

--- curriculum/class-12/demo/front-end/src/App.js
+++ curriculum/class-12/demo/front-end/src/App.js
@@ -1,40 +1,32 @@
 import React from 'react';
 import axios from 'axios';
 import Cats from './Cats';
-import Form from './Form';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       cats: [],
-      name: ''
     }
   }
 
-  getMyCats = async (e) => {
-    e.preventDefault();
-    const SERVER = 'http://localhost:3001';
-    try {
-      const cats = await axios.get(`${SERVER}/cats`, {params: { name: this.state.name }});
-      console.log(cats.data)
-      this.setState({ cats: cats.data });
+  async componentDidMount() {
+    // const SERVER = 'http://localhost:3001';
+    const SERVER = process.env.REACT_APP_SERVER;
 
-    } catch(error){
+    try {
+      const response = await axios.get(`${SERVER}/cats`);
+      this.setState({ cats: response.data });
+
+    } catch (error) {
       console.log(error);
     }
   }
 
-  updateName = (name) => this.setState({ name });
-
   render() {
-    return(
+    return (
       <>
         <Cats cats={this.state.cats} />
-        <Form 
-        updateName={this.updateName} 
-        getMyCats={this.getMyCats}
-        />
       </>
     )
   }
