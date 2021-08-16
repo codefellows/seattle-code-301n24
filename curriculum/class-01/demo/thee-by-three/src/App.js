9c35100af96e9ea1609fc998f266d3f4413fdfdf
JB-Tellez <jb.tellez@gmail.com>
2021-08-16 15:36:31 -0700

    demo tweaks to match in class comments

--- curriculum/class-01/demo/thee-by-three/src/App.js
+++ curriculum/class-01/demo/thee-by-three/src/App.js
@@ -1,5 +1,6 @@
-import { Component } from 'react'
+import Grid from './grid'
 import './App.css'
+
 function App() {
   return (
     <div className="App">
@@ -7,34 +8,6 @@ function App() {
       <Grid />
     </div>
   );
-}
-
-class Grid extends Component {
-  render() {
-    return <div className="grid">
-      <Row rowNum={0} />
-      <Row rowNum={1} />
-      <Row rowNum={2} />
-    </div>
-  }
-}
-
-function Row(props) {
-  const startSquareValue = (props.rowNum * 3) + 1;
-
-  return (
-    <div className="row">
-      <Square value={startSquareValue} />
-      <Square value={startSquareValue + 1} />
-      <Square value={startSquareValue + 2} />
-    </div>
-  )
-}
-
-class Square extends Component {
-  render() {
-    return <button className="square">{this.props.value}</button>
-  }
 }
 
 export default App;
