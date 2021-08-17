3f8489b6db2610f90c6689d2452bd8e0c4e69380
JB-Tellez <jb.tellez@gmail.com>
2021-08-17 14:56:14 -0700

    adds navbar to header

--- curriculum/class-02/demo/beast-state/src/header.js
+++ curriculum/class-02/demo/beast-state/src/header.js
@@ -1,3 +1,12 @@
+import Navbar from 'react-bootstrap/Navbar'
+
 export default function Header(props) {
-  return <header><h1>{props.title}</h1></header>
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand>
+        <h1>{props.title}</h1>
+      </Navbar.Brand>
+    </Navbar>
+
+  );
 }
