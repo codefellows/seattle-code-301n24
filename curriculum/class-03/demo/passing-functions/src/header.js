10793b5d11dd9febaceb19ebc4e2f28cba77ecf4
JB-Tellez <jb.tellez@gmail.com>
2021-08-17 18:10:01 -0700

    brings over previous demo content

---
+++ curriculum/class-03/demo/passing-functions/src/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">{this.props.title}</Navbar.Brand>
+      </Navbar>
+    )
+  }
+}
+
+export default Header;
