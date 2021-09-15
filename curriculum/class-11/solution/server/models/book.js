016d60306495491a97dd10cb5154ae4af52475d6
JB-Tellez <jb.tellez@gmail.com>
2021-09-15 15:59:01 -0700

    updates lab 11 server solution to work with labs 11,12 and 13

--- curriculum/class-11/solution/server/models/book.js
+++ curriculum/class-11/solution/server/models/book.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: {type: String, required: true}, 
+    email: { type: String, required: true },
     description: { type: String, required: true },
-    status: { type: String, uppercase: true, enum: ['LIFE-CHANGING', 'FAVORITE FIVE', 'RECCOMENDED TO ME'] },
+    status: { type: String, uppercase: true, enum: ['LIFE-CHANGING', 'FAVORITE FIVE', 'RECOMMENDED TO ME'] },
 });
 
 
