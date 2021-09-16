c2becf2adb290b93c718b9f5f278d1e8ac67f7ef
JB-Tellez <jb.tellez@gmail.com>
2021-09-15 22:36:56 -0700

    adds update functionality to latest front end solutions

--- curriculum/class-13/solution/front-end/src/BookFormModal.js
+++ curriculum/class-13/solution/front-end/src/BookFormModal.js
@@ -1,106 +1,77 @@
-import React from 'react';
-import Modal from 'react-bootstrap/Modal';
+import { Component } from 'react';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-import { withAuth0 } from '@auth0/auth0-react';
 
-const API = 'http://localhost:3001';
 
-class BookFormModal extends React.Component {
-  constructor(props){
+class BookFormModal extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      name: !this.props.newBook ? this.props.book.name : '',
-      description: !this.props.newBook ? this.props.book.description :  '',
-      status: !this.props.newBook ? this.props.book.status : 'LIFE-CHANGING',
-      img: !this.props.newBook ? this.props.book.img : ''
-    }
+
+    this.state = {
+      name: props.book?.name || '',
+      description: props.book?.description || '',
+      status: props.book?.status || 'LIFE-CHANGING',
+      method: props.book ? 'put' : 'post',
+      title: props.book ? 'Update Favorite' : 'Add Book to Favorites'
+    };
   }
+
 
   handleClose = () => {
     this.props.close();
   }
 
-  createBook = () => {
-    this.props.auth0.getIdTokenClaims()
-    .then(async res => {
-      const jwt = res.__raw;
+  createBook = async () => {
 
-      const config = {
-        headers: {"Authorization" : `Bearer ${jwt}`},
-        data: {
-          email: this.props.auth0.user.email,
-          name: this.state.name,
-          description: this.state.description,
-          status: this.state.status,
-          img: this.state.img,
-        },
-        baseURL: process.env.REACT_APP_SERVER,
-        url: '/books',
-        method: 'post'
+    const config = {
+      method: this.state.method,
+      baseURL: process.env.REACT_APP_SERVER,
+      url: '/books/',
+      data: {
+        email: this.props.user.email,
+        name: this.state.name,
+        description: this.state.description,
+        status: this.state.status,
       }
-      
-      const bookResults = await axios(config);
-      this.props.close();
-      this.props.updateBookArray(bookResults.data);
-    })
-    .catch(err => console.error(err));
-  }
-  
-  updateBook = async() => {
-    this.props.auth0.getIdTokenClaims()
-    .then(async res => {
-      const jwt = res.__raw;
-      
-      const config = {
-        headers: {"Authorization" : `Bearer ${jwt}`},
-        data: { 
-          email: this.props.auth0.user.email, 
-          name: this.state.name, 
-          description: this.state.description, 
-          status: this.state.status,
-          img: this.state.img
-        },
-        method: 'put',
-        baseURL: process.env.REACT_APP_SERVER,
-        url: `/books/${this.props.index}`
-      }
+    }
 
-      const updatedBookObj = await axios(config);
-  
-      this.props.updateBookArray(updatedBookObj.data);
-      this.props.close();
-    });
-  }
+    if (this.state.method === 'put') {
+      config.url = `/books/${this.props.book._id}/?email=${this.props.user.email}`
+    }
+
+    const bookResults = await axios(config);
+    this.props.close();
+    this.props.updateBookArray(bookResults.data);
+
+  };
+
 
   render() {
-    return(
+    return (
       <Modal show={this.props.show} onHide={this.handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{this.props.newBook ? 'Add Book To Favorites' : 'Update Book'}</Modal.Title>
+          <Modal.Title>{this.state.title}</Modal.Title>
         </Modal.Header>
         <Form>
           <Form.Group controlId="name">
             <Form.Label>Book Name</Form.Label>
-            <Form.Control onChange={(e)=> this.setState({name:e.target.value})} type="text" placeholder={this.props.newBook ? "book name" : this.props.book.name} />
+            <Form.Control onChange={(e) => this.setState({ name: e.target.value })} type="text" placeholder="book name" defaultValue={this.state.name} />
           </Form.Group>
           <Form.Group controlId="description">
             <Form.Label>Book description</Form.Label>
-            <Form.Control onChange={(e)=> this.setState({description:e.target.value})} type="text" placeholder={this.props.newBook ? "book description" : this.props.book.description} />
+            <Form.Control onChange={(e) => this.setState({ description: e.target.value })} type="text" placeholder="book description" defaultValue={this.state.description} />
           </Form.Group>
-          <Form.Group controlId="image">
-            <Form.Label>Book Image URL</Form.Label>
-            <Form.Control onChange={(e)=> this.setState({img:e.target.value})} type="text" placeholder={this.props.newBook ? "book image url" : this.props.book.img} />
-          </Form.Group>
+
           <Form.Group controlId="status">
             <Form.Label>Status</Form.Label>
-            <Form.Control defaultValue='' onChange={(e)=> this.setState({status:e.target.value})} as="select">
+            <Form.Control defaultValue={this.state.status} onChange={(e) => this.setState({ status: e.target.value })} as="select">
               <option></option>
               <option value="LIFE-CHANGING">Life Changing</option>
               <option value="FAVORITE FIVE">Favorite Five</option>
-              <option value="RECCOMENDED TO ME">Reccomended To Me</option>
+              <option value="RECOMMENDED TO ME">Recommended To Me</option>
             </Form.Control>
           </Form.Group>
         </Form>
@@ -108,7 +79,7 @@ class BookFormModal extends React.Component {
           <Button variant="secondary" onClick={this.handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={this.props.newBook ? this.createBook : this.updateBook}>
+          <Button variant="primary" onClick={this.createBook}>
             Save Changes
           </Button>
         </Modal.Footer>
@@ -117,4 +88,4 @@ class BookFormModal extends React.Component {
   }
 }
 
-export default withAuth0(BookFormModal);
+export default BookFormModal;
