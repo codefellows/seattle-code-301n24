da2773d527327c245e5883aacab9f6c8e58d7b8f
Hexx King <hexxking13@gmail.com>
2021-12-21 21:04:19 -0800

    updated demo to include a container

--- class-04/demo/App.js
+++ class-04/demo/App.js
@@ -1,50 +1,47 @@
-import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Component } from "react";
+import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
-import './App.css';
 
 const allNumerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-class App extends React.Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      nums: allNumerals
-    }
+    this.state = { numerals: allNumerals };
   }
 
-  handleChange = (e) => {
-    console.log(e.target.value);
-    const selection = e.target.value;
-    let updatedNums;
+  handleChange = (event) => {
+    const selection = event.target.value;
+    let updatedNumerals;
 
     if (selection === "odd") {
-      updatedNums = allNumerals.filter(num => num % 2 === 1);
-      this.setState({nums: updatedNums});
+      updatedNumerals = allNumerals.filter((numeral) => numeral % 2 === 1);
+      this.setState({ numerals: updatedNumerals });
     } else if (selection === "even") {
-      updatedNums = allNumerals.filter(num => num % 2 === 0);
-      this.setState({nums: updatedNums});
+      updatedNumerals = allNumerals.filter((numeral) => numeral % 2 === 0);
+      this.setState({ numerals: updatedNumerals });
     } else {
-      this.setState({nums: allNumerals});
+      this.setState({ numerals: allNumerals });
     }
-  }
+  };
 
   render() {
     return (
-      <div className="App">
+      <Container>
         <Form>
-          <Form.Label>Numeral Filter</Form.Label>
-          <br/>
-          <Form.Select onChange={this.handleChange}>
-            <option value="none">Unfiltered Numbers</option>
-            <option value="odd">Filtered Odd Numbers</option>
-            <option value="even">Filtered Even Numbers</option>
+          <Form.Label>Filter Numerals</Form.Label>
+          <Form.Select
+            onChange={this.handleChange}
+            aria-label="Default select example"
+          >
+            <option value="none">Unfiltered</option>
+            <option value="odd">Odds</option>
+            <option value="even">Evens</option>
           </Form.Select>
         </Form>
-        <p>{this.state.nums}</p>
-      </div>
-  );
+        <p>{this.state.numerals}</p>
+      </Container>
+    );
+  }
 }
-}
-
-export default App;
